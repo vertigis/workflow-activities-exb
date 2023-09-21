08d8adcf78be9469eace966b2750735d2cdb1407
Michael Kowal <mkowal@latitudegeo.com>
2023-09-21 15:11:59 -0700

    Sort activity imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 // Activities will be re-exported from this file.
-
-
-
-export { default as SelectFeatures } from "./activities/SelectFeatures";
-
 export { default as ClearSelectedFeatures } from "./activities/ClearSelectedFeatures";
+export { default as SelectFeatures } from "./activities/SelectFeatures";
