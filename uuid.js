bd9b38b9d1f07ee7eab4d3da04a8564156d1888e
Michael Kowal <35471076+MichaelKowal@users.noreply.github.com>
2024-01-31 13:45:12 -0800

    fix!: Fix UUID (#7)

--- uuid.js
+++ uuid.js
@@ -2,6 +2,6 @@
 
 // This is used to keep track of your unique project id.
 // Do not modify.
-const uuid = "workflow-activities-exb";
+const uuid = "0790222e-6d41-4410-a391-af04e8b5ae5b";
 
 module.exports = uuid;
