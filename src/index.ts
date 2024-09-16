7951f6c4a64591ddf24dae1bdde5e68557a7ab64
Michael Kowal <35471076+MichaelKowal@users.noreply.github.com>
2024-09-16 12:15:27 -0700

    feat: Add open widget activity stub (#13)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 // Activities will be re-exported from this file.
 export { default as ClearSelectedFeatures } from "./activities/ClearSelectedFeatures";
 export { default as SelectFeatures } from "./activities/SelectFeatures";
+export { default as OpenWidgetActivity } from "./activities/OpenWidget";
