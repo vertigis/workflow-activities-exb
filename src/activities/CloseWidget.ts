846e3561f5ca38002560a7d29af5ee1594b67da4
Michael Kowal <mkowal@latitudegeo.com>
2024-09-18 10:30:10 -0700

    PR Fixes

--- src/activities/CloseWidget.ts
+++ src/activities/CloseWidget.ts
@@ -12,13 +12,13 @@ interface CloseWidgetInputs {
 }
 /**
  * @displayName Close Widget
- * @category Custom Activities
- * @description Close a widget in an Experience Builder app.
+ * @category Experience Builder
+ * @description Closes an Experience Builder widget.
  */
 export default class CloseWidget implements IActivityHandler {
     /** Perform the execution logic of the activity. */
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     execute(inputs: CloseWidgetInputs): Promise<void> {
-        throw new ActivityNotImplementedError("OpenWidget");
+        throw new ActivityNotImplementedError("CloseWidget");
     }
 }
